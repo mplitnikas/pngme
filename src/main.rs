cc2fe5dc0c0bf080dcdedebd7add237f817fe9a8
Matt Plitnikas <mplitnikas@gmail.com>
2023-09-20 02:31:46 -0400

    add clap crate and stub out main.rs and args.rs to parse CLI args

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,17 @@ mod chunk;
 mod chunk_type;
 mod commands;
 mod png;
+use crate::args::Args;
+use clap::Parser;
 
 pub type Error = Box<dyn std::error::Error>;
 pub type Result<T> = std::result::Result<T, Error>;
 
 fn main() -> Result<()> {
-    todo!()
+    let args = Args::parse();
+
+    for _ in 0..args.count {
+        println!("Hello {}!", args.name)
+    }
+    Ok(())
 }
